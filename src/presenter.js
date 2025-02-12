06838b3b972f60e7c4de2f6631125d316223b7cc
JaimeTadeo <deugartebutron@gmail.com>
2025-02-12 00:28:29 -0400

    anadido multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,20 @@
-import sumar from "./sumador";
+import sumar from "./sumador.js";
+import multiplicar from "./multiplicador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const sumaButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
+sumaButton.addEventListener("click", () => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  div.innerHTML = `<p>Resultado: ${sumar(firstNumber, secondNumber)}</p>`;
+});
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  div.innerHTML = `<p>Resultado: ${multiplicar(firstNumber, secondNumber)}</p>`;
 });
